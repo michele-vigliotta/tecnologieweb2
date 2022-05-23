f042c0332994c22b21fc295955f1c5f835e3ac24
Diego30090 <diego.vaccarini@gmail.com>
2022-05-23 16:22:07 +0200

    fixed navbar public pages

--- www/laraProject/resources/views/faq.blade.php
+++ www/laraProject/resources/views/faq.blade.php
@@ -56,21 +56,21 @@
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav  ">
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('index') }}">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('about') }}"> About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('catalogo') }}">Catalogo</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('why') }}">Why Us</a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="{{ route('testimonial') }}">Testimonial <span class="sr-only">(current)</span></a>
-              </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ route('index') }}"> Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('catalogo') }}"> Catalogo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about') }}"> About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('why') }}"> Why Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('faq') }}">FAQ</a>
+                </li>
             </ul>
             @if(isset(Auth::user()->nome))
               <div class="quote_btn-container">
