c8ab8e5d795fa7af3572e6f7c39c5eb633cd77d3
michele-vigliotta <michele.vigliotta00@gmail.com>
2022-05-25 14:47:53 +0200

    fix homeutente

--- www/laraProject/resources/views/homeutente.blade.php
+++ www/laraProject/resources/views/homeutente.blade.php
@@ -56,41 +56,36 @@
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav  ">
-              <li class="nav-item active">
-                <a class="nav-link" href="{{ route('index') }}">Home <span class="sr-only">(current)</span></a>
+               <ul class="navbar-nav  ">
+                 <li class="nav-item">
+                    <a class="nav-link" href="{{ route('index') }}"> Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ route('catalogo') }}"> Catalogo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about') }}"> About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('why') }}"> Why Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('faq') }}">FAQ</a>
+                </li>
+            @if(isset(Auth::user()->nome))
+              <li class="nav-item">
+                <a class="nav-link" href="">Chat</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="{{ route('catalogo') }}">Catalogo</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('faq') }}">Faq</a>
-              </li>
-              @if(isset(Auth::user()->nome))
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('homeutente') }}">Chat</a>
-              </li>
-              <li class="nav-item">
-<<<<<<< HEAD
                   <a class="nav-link" href="{{ route('profile')}}">Profilo</a>
               </li>
-
-                    @if('Locatore'==(Auth::user()->tipo))
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('catalogo')}}">Annunci</a>
-                  <a class="nav-link" href="{{ route('catalogo') }}">Profilo</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('catalogo') }}">Annunci</a>
-=======
-                <a class="nav-link" href="{{ route('profile')}}">Profilo</a>
-              </li>
+              
                     @if('Locatore'==(Auth::user()->tipo))
               <li class="nav-item">
                   <a class="nav-link" href="">Annunci</a>
->>>>>>> 2d7002b070b2c25f099ade77e5ce8f52e3456334
               </li>
                     @endif
-              @endif
+                @endif
              </ul>
             @if(isset(Auth::user()->nome))
               <div class="quote_btn-container">
