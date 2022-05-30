5ff138822e010092694b891fb1089a1eb452b924
Diego30090 <diego.vaccarini@gmail.com>
2022-05-30 20:39:08 +0200

    Refactory js under footer

--- www/laraProject/resources/views/signup.blade.php
+++ www/laraProject/resources/views/signup.blade.php
@@ -269,20 +269,7 @@
   </footer>
   <!-- footer section -->
 
-  <!-- jQery -->
-  <script src="{{ URL('js/jquery-3.4.1.min.js') }}"></script>
-  <!-- popper js -->
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <!-- nice select -->
-  <script src="{{ URL('js/jquery.nice-select.min.js') }}"></script>
-  <!-- bootstrap js -->
-  <script src="{{ URL('js/bootstrap.js') }}"></script>
-  <!-- owl slider -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-  <!-- phone prefix -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
-  <!-- custom js -->
-  <script src="{{ URL('js/custom.js') }}"></script>
+  @include('includes.jsScript')
 
 
 </body>
