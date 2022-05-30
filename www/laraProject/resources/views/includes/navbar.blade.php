289868678c1b5119049b57225bc9f9480a32c1ca
Diego30090 <diego.vaccarini@gmail.com>
2022-05-30 23:06:20 +0200

    Navbar Update

--- www/laraProject/resources/views/includes/navbar.blade.php
+++ www/laraProject/resources/views/includes/navbar.blade.php
@@ -9,48 +9,104 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav  ">
-                    <!-- Route Index -->
-                    @if( Request::is('test2'))
-                        <li class="nav-item active">
-                    @else
-                        <li class="nav-item">
-                    @endif
-                        <a class="nav-link" href="{{ route('index') }}"> Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <!-- Route Catalogo -->
-                    @if( Request::is('catalogo'))
-                        <li class="nav-item active">
-                    @else
+        <!-- Route Index -->
+        @if( Request::is('index')||Request::is('test2'))
+            <li class="nav-item active">
+        @else
+            <li class="nav-item">
+        @endif
+                <a class="nav-link" href="{{ route('index') }}"> Home <span class="sr-only">(current)</span></a>
+            </li>
+        <!-- Route Catalogo -->
+        @if( Request::is('catalogo'))
+            <li class="nav-item active">
+        @else
+            <li class="nav-item">
+        @endif
+                <a class="nav-link" href="{{ route('catalogo') }}"> Catalogo</a>
+            </li>
+
+
+        @if(isset(Auth::user()->nome))
+        @else
+
+        <!-- Route About -->
+            @if( Request::is('about'))
+                <li class="nav-item active">
+            @else
+                <li class="nav-item">
+            @endif
+                    <a class="nav-link" href="{{ route('about') }}"> About</a>
+                </li>
+        <!-- Route Why -->
+            @if( Request::is('why'))
+                <li class="nav-item active">
+            @else
+                <li class="nav-item">
+            @endif
+                    <a class="nav-link" href="{{ route('why') }}"> Why Us</a>
+                </li>
+            @endif
+
+
+        <!-- Route Faq -->
+            @if( Request::is('faq'))
+                <li class="nav-item active">
+            @else
+                <li class="nav-item">
+            @endif
+                    <a class="nav-link" href="{{ route('faq') }}">FAQ</a>
+                </li>
+
+
+        <!-- Route Chat -->
+        @if(isset(Auth::user()->nome))
+            @if('locatario'==(Auth::user()->tipo)||'locatore'==(Auth::user()->tipo))
+                @if( Request::is('chat'))
+                    <li class="nav-item active">
+                @else
                     <li class="nav-item">
-                    @endif
-                        <a class="nav-link" href="{{ route('catalogo') }}"> Catalogo</a>
+                @endif
+                        <a class="nav-link" href="{{ route('chat') }}">Chat</a>
                     </li>
-                    <!-- Route About -->
-                    @if( Request::is('about'))
-                        <li class="nav-item active">
-                    @else
-                        <li class="nav-item">
-                    @endif
-                        <a class="nav-link" href="{{ route('about') }}"> About</a>
-                    </li>
-                        <!-- Route Why -->
-                    @if( Request::is('why'))
-                        <li class="nav-item active">
-                    @else
-                        <li class="nav-item">
-                    @endif
-                        <a class="nav-link" href="{{ route('why') }}"> Why Us</a>
-                    </li>
-                    <!-- Route Faq -->
-                    @if( Request::is('faq'))
-                        <li class="nav-item active">
-                    @else
+            @endif
+        @endif
+
+
+        <!-- Route Annunci -->
+        @if(isset(Auth::user()->nome))
+            @if('locatore'==(Auth::user()->tipo))
+                @if( Request::is('annunci'))
+                    <li class="nav-item active">
+                @else
                     <li class="nav-item">
-                    @endif
-                        <a class="nav-link" href="{{ route('faq') }}">FAQ</a>
+                @endif
+                        <a class="nav-link" href="{{ route('annunci') }}">Annunci</a>
                     </li>
-                </ul>
-            @if(isset(Auth::user()->nome))
+            @endif
+        @endif
+
+                <!-- Route Statistiche -->
+                @if(isset(Auth::user()->nome))
+                    @if('admin'==(Auth::user()->tipo))
+                        @if( Request::is('stats'))
+                            <li class="nav-item active">
+                        @else
+                            <li class="nav-item">
+                                @endif
+                                <a class="nav-link" href="{{ route('stats') }}">stats</a>
+                            </li>
+                        @endif
+                    @endif
+
+
+
+
+
+
+        </ul>
+        <!-- Login/Register Route -->
+        @if(isset(Auth::user()->nome))
             <div class="quote_btn-container">
                 <a href="{{ route('profile') }}" class="quote_btn">
                     {{ Auth::user()->nome }}
