8a074a2406f94d8ac78c7a6272f9a81816d9c743
michele-vigliotta <michele.vigliotta00@gmail.com>
2022-05-31 17:25:39 +0200

    added homeadmin

---
+++ www/laraProject/resources/views/homeadmin.blade.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  @include('includes/head')
+</head>
+
+<body>
+
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="{{ route('index') }}">
+            <span>
+              Affitta casa
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  ">
+               <li class="nav-item">
+                    <a class="nav-link" href="{{ route('index') }}"> Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('catalogo') }}"> Catalogo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about') }}"> About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('why') }}"> Why Us</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ route('faq') }}">FAQ</a>
+                </li>
+            @if(isset(Auth::user()->nome))
+              <li class="nav-item">
+                <a class="nav-link" href="">Chat</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="{{ route('profile')}}">Profilo</a>
+              </li>
+
+                    @if('Locatore'==(Auth::user()->tipo))
+              <li class="nav-item">
+                  <a class="nav-link" href="">Annunci</a>
+              </li>
+                    @endif
+                @endif
+             </ul>
+            @if(isset(Auth::user()->nome))
+              <div class="quote_btn-container">
+                <a href="{{ route('homeutente') }}" class="quote_btn">
+                  {{ Auth::user()->nome }}
+                </a>
+              </div>
+              &nbsp
+              <div class="quote_btn-container">
+                <a href="{{ route('logout') }}" class="quote_btn">
+                  LOGOUT
+                </a>
+              </div>
+            @else
+              <div class="quote_btn-container">
+                <a href="{{ route('login') }}" class="quote_btn">
+                  LOG IN
+                </a>
+              </div>
+              &nbsp;
+              <div class="quote_btn-container">
+                <a href="{{ route('signup') }}" class="quote_btn">
+                  SIGN UP
+                </a>
+              </div>
+            @endif
+
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+
+<div class="main">
+  <!-- welcome section -->
+  <section class="about_section layout_padding3">
+    <div class="container  ">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="detail-box">
+            <div class="heading_container">
+              <h2>
+                Bentornato {{Auth::user()->nome}}!
+              </h2>
+            </div>
+            @if('Locatore'==(Auth::user()->tipo))
+            <p>
+              Inizia ora a pubblicare un annuncio!
+            </p>
+            <a href="{{route('aggAnnuncio')}}">
+              Pubblica ora
+            </a>
+            @else
+            <p>
+              Trova l'annuncio più adatto a te
+            </p>
+            <a href="{{ route('catalogo') }}">
+              Sfoglia catalogo
+            </a>
+            @endif
+          </div>
+        </div>
+        <div class="col-md-6 ">
+          <div class="img-box">
+            <img src="{{ URL('images/about-img.jpg')}}" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end about section -->
+
+
+
+  <!-- why us section -->
+  <section class="why_us_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Why Choose Us?
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-md-4 col-sm-6 mx-auto">
+          <div class="box">
+            <div class="img-box">
+              <img src="{{ URL('images/w1.png') }}" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Satisfied customers
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC,
+                making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in
+                Virginia.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6 mx-auto">
+          <div class="box">
+            <div class="img-box">
+              <img src="{{ URL('images/w2.png') }}" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                24/7 Support
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC,
+                making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in
+                Virginia.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6 mx-auto">
+          <div class="box">
+            <div class="img-box">
+              <img src="{{ URL('images/w3.png') }}" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Affordable price
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC,
+                making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in
+                Virginia.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="btn-box">
+        <a href="">
+          Read More
+        </a>
+      </div>
+    </div>
+  </section>
+  <!-- end why us section -->
+
+  <!-- client section -->
+  <section class="client_section layout_padding-bottom">
+    <div class="container">
+      <div class="heading_container heading_center psudo_white_primary mb_45">
+        <h2>
+
+        </h2>
+      </div>
+      <div class="carousel-wrap ">
+        <div class="owl-carousel client_owl-carousel">
+          <div class="item">
+            <div class="box">
+              <div class="img-box">
+                <img src="{{ URL('images/client1.jpg') }}" alt="" class="box-img">
+              </div>
+              <div class="detail-box">
+                <div class="client_id">
+                  <div class="client_info">
+                    <h6>
+                      LusDen
+                    </h6>
+                    <p>
+                      magna aliqua. Ut
+                    </p>
+                  </div>
+                  <i class="fa fa-quote-left" aria-hidden="true"></i>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="box">
+              <div class="img-box">
+                <img src="{{ URL('images/client2.jpg') }}" alt="" class="box-img">
+              </div>
+              <div class="detail-box">
+                <div class="client_id">
+                  <div class="client_info">
+                    <h6>
+                      Zen Court
+                    </h6>
+                    <p>
+                      magna aliqua. Ut
+                    </p>
+                  </div>
+                  <i class="fa fa-quote-left" aria-hidden="true"></i>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="box">
+              <div class="img-box">
+                <img src="{{ URL('images/client1.jpg') }}" alt="" class="box-img">
+              </div>
+              <div class="detail-box">
+                <div class="client_id">
+                  <div class="client_info">
+                    <h6>
+                      LusDen
+                    </h6>
+                    <p>
+                      magna aliqua. Ut
+                    </p>
+                  </div>
+                  <i class="fa fa-quote-left" aria-hidden="true"></i>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="box">
+              <div class="img-box">
+                <img src="{{ URL('images/client2.jpg') }}" alt="" class="box-img">
+              </div>
+              <div class="detail-box">
+                <div class="client_id">
+                  <div class="client_info">
+                    <h6>
+                      Zen Court
+                    </h6>
+                    <p>
+                      magna aliqua. Ut
+                    </p>
+                  </div>
+                  <i class="fa fa-quote-left" aria-hidden="true"></i>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end client section -->
+
+    @include('contentSections/general/infoSection')
+
+
+    @include('includes/footer')
+    @include('includes.jsScript')
+</div>
+
+</body>
+</html>
