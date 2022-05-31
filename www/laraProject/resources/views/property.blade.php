91b4f4d10a32eb41415c65a542ce6064422ce954
Diego30090 <diego.vaccarini@gmail.com>
2022-05-31 19:37:12 +0200

    Inserted fixed Navbar

--- www/laraProject/resources/views/property.blade.php
+++ www/laraProject/resources/views/property.blade.php
@@ -11,50 +11,7 @@
     <!-- header section strats -->
     <header class="header_section">
       <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg custom_nav-container ">
-          <a class="navbar-brand" href="index.html">
-            <span>
-              Estee
-            </span>
-          </a>
-
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class=""> </span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav  ">
-              <li class="nav-item ">
-                <a class="nav-link" href="{{ route('index') }}">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('about') }}"> About</a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="{{ route('property') }}">Property</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('why') }}">Why Us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('testimonial') }}">Testimonial</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"> Login</a>
-              </li>
-              <form class="form-inline">
-                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                  <i class="fa fa-search" aria-hidden="true"></i>
-                </button>
-              </form>
-            </ul>
-            <div class="quote_btn-container">
-              <a href="" class="quote_btn">
-                Get A Quote
-              </a>
-            </div>
-          </div>
-        </nav>
+          @include('includes/navbar')
       </div>
     </header>
     <!-- end header section -->
