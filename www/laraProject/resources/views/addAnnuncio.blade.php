21dd4c3b92a1b4c669266fe2a4fe10f587c559d4
Diego30090 <diego.vaccarini@gmail.com>
2022-05-31 19:32:30 +0200

    Updated addAnnuncio

--- www/laraProject/resources/views/addAnnuncio.blade.php
+++ www/laraProject/resources/views/addAnnuncio.blade.php
@@ -2,41 +2,7 @@
 <html>
 
 <head>
-  <!-- Basic -->
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!-- Mobile Metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Site Metas -->
-  <meta name="keywords" content="" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
-
-  <title>Affitta casa</title>
-
-
-  <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="{{ URL('css/bootstrap.css') }}" />
-
-  <!-- fonts style -->
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
-
-  <!-- nice selecy -->
-  <link rel="stylesheet" href="{{ URL('css/nice-select.min.css') }}">
-
-  <!--owl slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-
-  <!-- phone prefix -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css"/>
-
-  <!-- font awesome style -->
-  <link href="{{ URL('css/font-awesome.min.css')}}" rel="stylesheet" />
-
-  <!-- Custom styles for this template -->
-  <link href="{{ URL('css/style.css') }}" rel="stylesheet" />
-  <!-- responsive style -->
-  <link href="{{ URL('css/responsive.css') }}" rel="stylesheet" />
+@include('includes/head')
 
 </head>
 
