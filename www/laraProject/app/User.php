0cba3d0949ed647e4f8ca45d4a33a70d4b197034
marcopmartino <s1092106@studenti.univpm.it>
2022-06-01 18:22:49 +0200

    Change user.php

--- www/laraProject/app/User.php
+++ www/laraProject/app/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
 
     public function setPasswordAttribute($password)
     {
-        $this->attributes['password'] = bcrypt($password);
+        $this->attributes['password'] = Hash::make($password);
     }
 
     public function messaggio(){
