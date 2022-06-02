c789375879ebc929a6a1ee7ef641dcfde3e21842
Diego30090 <diego.vaccarini@gmail.com>
2022-06-02 18:09:50 +0200

    Complete formatter why page

--- www/laraProject/resources/views/why.blade.php
+++ www/laraProject/resources/views/why.blade.php
@@ -1,30 +1,10 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.default')
 
-<head>
-    @include('includes.head')
-</head>
+@section('carousel')
+@stop
 
-<body class="sub_page">
-
-  <div class="hero_area2">
-    <!-- header section strats -->
-    <header class="header_section">
-      <div class="container-fluid">
-        @include('includes/navbar')
-      </div>
-    </header>
-    <!-- end header section -->
-  </div>
-  <div id="main">
-    <!-- end why us section -->
-      @include('contentSections/general/whyUs')
-
-      @include('contentSections/general/infoSection')
-
-      @include('includes/footer')
-      @include('includes.jsScript')
-  </div>
-</body>
-
-</html>
+@section('content')
+    @include('contentSections/general/whyUs')
+    @include('contentSections/general/infoSection')
+    @include('contentSections.general.infoSection')
+@stop
