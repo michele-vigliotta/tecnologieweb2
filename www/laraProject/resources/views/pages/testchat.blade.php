fb09663740540509d57a2cae88d8cd880fd5a1a0
Diego30090 <diego.vaccarini@gmail.com>
2022-06-03 10:18:31 +0200

    chat test development

---
+++ www/laraProject/resources/views/pages/testchat.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+@section('optionalStyle')
+
+@stop
+
+@section('content')
+<div class="container">
+        <div class="leftpane" style="background-color:#9BCB3B;">
+            <h1>try1</h1>
+        </div>
+        <div class="middlepane" style="background-color:#1AB99E;">
+            <h1>try1</h1>
+        </div>
+        <div class="rightpane" style="background-color:#F36F25;">
+            <h1>try1</h1>
+        </div>
+</div>
+@stop
